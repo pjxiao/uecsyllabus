f619d8c5696688e12796b26ae650245603995e33
Takayuki Hirai <gen@pjxiao.net>
2015-01-15 23:34:39 +0900

    Ready for UTF-8

--- bin/search.py
+++ bin/search.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
+import six
 import sys
+import codecs
 import argparse
 import uecsyllabus
+
+if six.PY2:
+    sys.stdout = codecs.getwriter("utf_8")(sys.stdout)
 
 parser = argparse.ArgumentParser(description="Search UEC syllabus")
 parser.add_argument("--course", required=True, help="Course ID")
@@ -13,5 +18,4 @@ args = parser.parse_args()
 finder = uecsyllabus.Finder()
 result = finder.search(vars(args))
 for row in result:
-    sys.stdout.write("\t".join(row) + "\n")
-
+    sys.stdout.write(("\t".join(row) + "\n"))
